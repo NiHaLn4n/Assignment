fe05e9b0ebcb19d6570f275e8afc76577afeffce
NiHaLn4n <nihaln4n@gmail.com>
2024-11-24 12:28:26 +0400

    Create exercise 2.py

---
+++ exercise 2.py
@@ -0,0 +1,9 @@
+# Declaring variable and its values as 8 and 10
+num1 = 8
+num2 = 10
+
+# Declaring a third variable that stores the sum of the first two numbers
+sum_result = num1 + num2
+
+# To print the value of the sum 
+print(sum_result)
