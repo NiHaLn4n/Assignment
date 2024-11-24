cb5bfe9d52ffa55b096d2673e15894094d187458
NiHaLn4n <nihaln4n@gmail.com>
2024-11-24 12:28:10 +0400

    Create exercise 1.py

---
+++ exercise 1.py
@@ -0,0 +1,14 @@
+# Exercise 1: Coding is cool
+
+# Define the first word
+word1 = "Coding"
+
+# Define the second word
+word2 = "is"
+
+# Define the third word
+word3 = "Cool"
+
+# Concatenate the words with spaces in between and print the result
+print(word1 + " " + word2 + " " + word3)
+
