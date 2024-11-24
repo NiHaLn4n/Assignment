0ce2c974078bed910bd2599616e3ef76298b5971
NiHaLn4n <nihaln4n@gmail.com>
2024-11-24 12:28:47 +0400

    Create exercise 7.py

---
+++ exercise 7.py
@@ -0,0 +1,27 @@
+# 1. Print numbers from 0 to 50
+for a in range(0, 51, 1): 
+    print(a)
+
+print()  # Blank line for space
+
+# 2. Print numbers from 50 to 0
+for b in range(50, -1, -1): 
+    print(b)
+
+print()  # Blank line for space
+
+# 3. Print numbers from 30 to 50
+for c in range(30, 51, 1): 
+    print(c)
+
+print()  # Blank line for space
+
+# 4. Print every second number from 50 to 10
+for d in range(50, 9, -2): 
+    print(d)
+
+print()  # Blank line for space
+
+# 5. Print numbers from 100 to 200 in steps of 5
+for e in range(100, 201, 5): 
+    print(e)
