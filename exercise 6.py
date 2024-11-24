899d8611d2400b43e02d338059fc0264a7291196
NiHaLn4n <nihaln4n@gmail.com>
2024-11-24 14:28:33 +0400

    Update exercise 6.py

--- exercise 6.py
+++ exercise 6.py
@@ -1,10 +1,14 @@
 #Exercise 6: Brute Force Attack
 
-#  Define the correct password
+# Define the correct password
 password = "12345"
 
-# Start an infinite loop
-while True:
+# Initialize the number of attempts
+attempts = 0
+max_attempts = 5  # Maximum number of allowed attempts
+
+# Start a loop that runs until the maximum number of attempts is reached
+while attempts < max_attempts:
     # Prompt the user to enter the password
     user_input = input("Enter your password: ")
 
@@ -13,4 +17,11 @@ while True:
         print("Access granted")
         break  # Exit the loop if the password is correct
     else:
-        print("Access denied. Try again!")  # Inform the user and continue the loop if the password is incorrect
+        attempts += 1  # Increment the number of attempts
+        remaining_attempts = max_attempts - attempts
+        if remaining_attempts > 0:
+            print(f"Access denied. Try again! You have {remaining_attempts} attempts left.")
+        else:
+            print("Access denied.maximum number of attempts reached.")
+            print("Authorities have been alerted.")  # Inform the user that authorities have been alerted
+            break  # Exit the loop
