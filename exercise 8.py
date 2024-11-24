a86d2adcdd923f0c52a67a2c1bb2c70b8cef681d
NiHaLn4n <nihaln4n@gmail.com>
2024-11-24 12:28:51 +0400

    Create exercise 8.py

---
+++ exercise 8.py
@@ -0,0 +1,12 @@
+# Create a list of names
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+# Prompt the user to enter a name to search for
+search_name = input("Enter name to search: ")
+
+# Check if the entered name is in the list
+if search_name in names:
+    print("Name found in list")
+else:
+    print("Name not found in list")
+
