e6c07797ff869b81c1fad37cfe1a659b8a15dbdf
NiHaLn4n <nihaln4n@gmail.com>
2024-11-24 12:28:32 +0400

    Create exercise 3.py

---
+++ exercise 3.py
@@ -0,0 +1,22 @@
+# for the user to enter details
+name = input("Enter your name: ")
+hometown = input("Enter your hometown: ")
+
+# To ensure that the age is an integer 
+while True:
+    age_input = input("Enter your age: ")
+    try:
+        age = int(age_input)
+        break
+    except ValueError:
+        print("Please enter a valid integer for age.")
+
+# storing the information in an dictionary
+person_info = {
+    "name": name,
+    "hometown": hometown,
+    "age": age
+}
+
+# Print the values on separate lines using a single print() statement
+print(f"\nName: {person_info['name']}\nHometown: {person_info['hometown']}\nAge: {person_info['age']}")
