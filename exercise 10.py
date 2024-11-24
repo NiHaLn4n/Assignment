9757ffdbb15d0b902e5bded5674a7063965a6dad
NiHaLn4n <nihaln4n@gmail.com>
2024-11-24 12:28:58 +0400

    Create exercise 10.py

---
+++ exercise 10.py
@@ -0,0 +1,14 @@
+# Define a function to check if a number is even or odd
+def check_even_or_odd(): 
+    # Prompt the user to enter a number and convert it to an integer
+    num = int(input("Enter a number: ")) 
+
+    # Check if the number is even
+    if num % 2 == 0: 
+        print("The number is even")
+    else: 
+        # If the number is not even, it is odd
+        print("The number is odd")
+
+# Call the function to execute it
+check_even_or_odd()
