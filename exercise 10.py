5fd901fffcbea18e3dc305be99c0d58c34e79c3e
NiHaLn4n <nihaln4n@gmail.com>
2024-11-24 14:19:12 +0400

    Update exercise 10.py

--- exercise 10.py
+++ exercise 10.py
@@ -1,3 +1,5 @@
+# Exercise 10: Is it even?
+
 # Define a function to check if a number is even or odd
 def check_even_or_odd(): 
     # Prompt the user to enter a number and convert it to an integer
